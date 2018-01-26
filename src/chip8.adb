bf9fc004467154fefdc11c31e4a39ff451668118
Gael Gilet--Couraud <gilet-_g@epita.fr>
2018-01-26 18:56:10 +0100

    instructions: implement pc-related private procedures and add the lsit of instruction handling functions.

--- src/chip8.adb
+++ src/chip8.adb
@@ -5,4 +5,16 @@ package body Chip8 is
       Chip.Opcode := Chip.ChipMemory[Chip.PC] << 8 | Chip.CMemory[PC + 1];
    end Fetch_Opcode;
 
+   procedure SetPc (cpu : in out Chip8; addr : in Address)
+   is
+   begin
+      cpu.PC := addr;
+   end SetPc;
+
+   procedure AddToPc (cpu : in out Chip8; offset : in Address)
+   is
+   begin
+      SetPc (cpu, (offset + cpu.PC) mod (Memory'Length));
+   end AddToPc;
+
 end Chip8;
