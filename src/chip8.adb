ff55e6bf82330485d3ae087ebf2709ab81457157
Gael Gilet--Couraud <gilet-_g@epita.fr>
2018-01-27 00:05:58 +0100

    cpu: add instructions up to SubVV.

--- src/chip8.adb
+++ src/chip8.adb
@@ -30,8 +30,9 @@ package body Chip8 is
    is
    begin
       if condition then
-         AddToPc (cpu, 2);
+         AddToPc (cpu, InstructionLength);
       end if;
+      AddToPc (cpu, InstructionLength);
    end ConditionalJump;
 
    procedure Jp (cpu : in out Chip8; instr : in InstructionBytes)
@@ -75,13 +76,90 @@ package body Chip8 is
    is
    begin
       ConditionalJump (cpu, cpu.V (Integer (instr (0) and 16#0f#)) =
-                            cpu.V (Integer (instr (1) and 16#f0#)));
+                            cpu.V (Integer (instr (1) and 16#f0#) / 16));
    end SeVV;
 
    procedure LdVB (cpu : in out Chip8; instr : in InstructionBytes)
    is
    begin
       cpu.V (Integer (instr (0) and 16#0f#)) := instr (1);
+      AddToPc (cpu, InstructionLength);
    end LdVB;
+
+   procedure AddVB (cpu : in out Chip8; instr : in InstructionBytes)
+   is
+      Index : Integer := Integer (instr (0) and 16#0f#);
+   begin
+      cpu.V (Index) := cpu.V (Index) + instr (1);
+      AddToPc (cpu, InstructionLength);
+   end AddVB;
+
+   procedure LdVV (cpu : in out Chip8; instr : in InstructionBytes)
+   is
+      Index : Integer := Integer (instr (0) and 16#0f#);
+      Index2 : Integer := Integer (instr (1) and 16#f0#) / 16;
+   begin
+      cpu.V (Index) := cpu.V (Index2);
+      AddToPc (cpu, InstructionLength);
+   end LdVV;
+
+   procedure OrVV (cpu : in out Chip8; instr : in InstructionBytes)
+   is
+      Index : Integer := Integer (instr (0) and 16#0f#);
+      Index2 : Integer := Integer (instr (1) and 16#f0#) / 16;
+   begin
+      cpu.V (Index) := cpu.V (Index) or cpu.V (Index2);
+      AddToPc (cpu, InstructionLength);
+   end OrVV;
+
+   procedure AndVV (cpu : in out Chip8; instr : in InstructionBytes)
+   is
+      Index : Integer := Integer (instr (0) and 16#0f#);
+      Index2 : Integer := Integer (instr (1) and 16#f0#) / 16;
+   begin
+      cpu.V (Index) := cpu.V (Index) and cpu.V (Index2);
+      AddToPc (cpu, InstructionLength);
+   end AndVV;
+
+   procedure XorVV (cpu : in out Chip8; instr : in InstructionBytes)
+   is
+      Index : Integer := Integer (instr (0) and 16#0f#);
+      Index2 : Integer := Integer (instr (1) and 16#f0#) / 16;
+   begin
+      cpu.V (Index) := cpu.V (Index) xor cpu.V (Index2);
+      AddToPc (cpu, InstructionLength);
+   end XorVV;
+
+   procedure AddVV (cpu : in out Chip8; instr : in InstructionBytes)
+   is
+      Index : Integer := Integer (instr (0) and 16#0f#);
+      Index2 : Integer := Integer (instr (1) and 16#f0#) / 16;
+      Overflow : Boolean := Integer (cpu.V (Index)) + Integer (cpu.V (Index2)) >
+                         255;
+   begin
+      if Overflow then
+         cpu.V (15) := 1;
+      else
+         cpu.V (15) := 0;
+      end if;
+      --  Standard is not clear about the order. Doing this second to have
+      --  same behaviour as the defined SubVV.
+      cpu.V (Index) := cpu.V (Index) + cpu.V (Index2);
+      AddToPc (cpu, InstructionLength);
+   end AddVV;
+
+   procedure SubVV (cpu : in out Chip8; instr : in InstructionBytes)   is
+      Index : Integer := Integer (instr (0) and 16#0f#);
+      Index2 : Integer := Integer (instr (1) and 16#f0#) / 16;
+      Positive : Boolean := Integer (cpu.V (Index)) > Integer (cpu.V (Index2));
+   begin
+      if Positive then
+         cpu.V (15) := 1;
+      else
+         cpu.V (15) := 0;
+      end if;
+      cpu.V (Index) := cpu.V (Index) - cpu.V (Index2);
+      AddToPc (cpu, InstructionLength);
+   end SubVV;
 
 end Chip8;
