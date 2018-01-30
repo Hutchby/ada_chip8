ffba13739ed5c4b719f10b05df5c80c81faa81fa
Ricimello Lucien <ricime_l@epita.fr>
2018-01-30 15:21:54 +0100

    gui: clickable button

--- src/gui.ads
+++ src/gui.ads
@@ -1,8 +1,11 @@
 with HAL.Bitmap; use HAL.Bitmap;
 
 package gui is
-   procedure pixel_draw (coord : Point);
+   procedure pixel_draw (coord : Point; b : Boolean);
    procedure pixel_clr (coord : Point);
    function get_touch return Point;
-   function get_button (coord : Point) return Natural;
+   function get_button (coord : Point) return Integer;
+   procedure draw_button;
+   procedure clr_screen;
+   procedure setup_screen;
 end gui;
