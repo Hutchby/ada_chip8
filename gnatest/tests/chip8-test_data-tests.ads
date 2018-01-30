5adb105e249e0a050e07d035fb0614115f66ede1
Maxime Gaudron <maxime.gaudron@epita.fr>
2018-01-30 18:37:37 +0100

    tests: add harness and tests source

---
+++ gnatest/tests/chip8-test_data-tests.ads
@@ -0,0 +1,136 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with Gnattest_Generated;
+
+package Chip8.Test_Data.Tests is
+
+   type Test is new GNATtest_Generated.GNATtest_Standard.Chip8.Test_Data.Test
+   with null record;
+
+   procedure Test_Initialize_8ce202 (Gnattest_T : in out Test);
+   --  chip8.ads:40:4:Initialize
+
+   procedure Test_FetchOpcode_994135 (Gnattest_T : in out Test);
+   --  chip8.ads:41:4:FetchOpcode
+
+   procedure Test_EmulateCycle_788153 (Gnattest_T : in out Test);
+   --  chip8.ads:42:4:EmulateCycle
+
+   procedure Test_Jp_1ac3cb (Gnattest_T : in out Test);
+   --  chip8.ads:44:4:Jp
+
+   procedure Test_Cls_1efc61 (Gnattest_T : in out Test);
+   --  chip8.ads:45:4:Cls
+
+   procedure Test_Ret_a5fc94 (Gnattest_T : in out Test);
+   --  chip8.ads:46:4:Ret
+
+   procedure Test_Call_aac095 (Gnattest_T : in out Test);
+   --  chip8.ads:47:4:Call
+
+   procedure Test_SeVB_101888 (Gnattest_T : in out Test);
+   --  chip8.ads:48:4:SeVB
+
+   procedure Test_SneVB_99e8d5 (Gnattest_T : in out Test);
+   --  chip8.ads:49:4:SneVB
+
+   procedure Test_SeVV_13d2d3 (Gnattest_T : in out Test);
+   --  chip8.ads:50:4:SeVV
+
+   procedure Test_LdVB_453c04 (Gnattest_T : in out Test);
+   --  chip8.ads:51:4:LdVB
+
+   procedure Test_AddVB_d51fed (Gnattest_T : in out Test);
+   --  chip8.ads:52:4:AddVB
+
+   procedure Test_LdVV_53aa27 (Gnattest_T : in out Test);
+   --  chip8.ads:53:4:LdVV
+
+   procedure Test_OrVV_f9f9ca (Gnattest_T : in out Test);
+   --  chip8.ads:54:4:OrVV
+
+   procedure Test_AndVV_7f3983 (Gnattest_T : in out Test);
+   --  chip8.ads:55:4:AndVV
+
+   procedure Test_XorVV_3a303e (Gnattest_T : in out Test);
+   --  chip8.ads:56:4:XorVV
+
+   procedure Test_AddVV_dea81c (Gnattest_T : in out Test);
+   --  chip8.ads:57:4:AddVV
+
+   procedure Test_SubVV_417abe (Gnattest_T : in out Test);
+   --  chip8.ads:58:4:SubVV
+
+   procedure Test_Shr_8593c5 (Gnattest_T : in out Test);
+   --  chip8.ads:59:4:Shr
+
+   procedure Test_SubN_2cf0cc (Gnattest_T : in out Test);
+   --  chip8.ads:60:4:SubN
+
+   procedure Test_Shl_d7daaf (Gnattest_T : in out Test);
+   --  chip8.ads:61:4:Shl
+
+   procedure Test_SneV_73e449 (Gnattest_T : in out Test);
+   --  chip8.ads:62:4:SneV
+
+   procedure Test_LdI_066ac8 (Gnattest_T : in out Test);
+   --  chip8.ads:63:4:LdI
+
+   procedure Test_JmpV_f874bb (Gnattest_T : in out Test);
+   --  chip8.ads:64:4:JmpV
+
+   procedure Test_Rnd_9b379c (Gnattest_T : in out Test);
+   --  chip8.ads:65:4:Rnd
+
+   procedure Test_Drw_b47b3a (Gnattest_T : in out Test);
+   --  chip8.ads:66:4:Drw
+
+   procedure Test_Skp_529e2c (Gnattest_T : in out Test);
+   --  chip8.ads:67:4:Skp
+
+   procedure Test_Sknp_600e10 (Gnattest_T : in out Test);
+   --  chip8.ads:68:4:Sknp
+
+   procedure Test_LdVT_4f0df5 (Gnattest_T : in out Test);
+   --  chip8.ads:69:4:LdVT
+
+   procedure Test_LdK_bd625b (Gnattest_T : in out Test);
+   --  chip8.ads:70:4:LdK
+
+   procedure Test_LdTV_05208e (Gnattest_T : in out Test);
+   --  chip8.ads:71:4:LdTV
+
+   procedure Test_LdSV_438265 (Gnattest_T : in out Test);
+   --  chip8.ads:72:4:LdSV
+
+   procedure Test_AddI_6793a9 (Gnattest_T : in out Test);
+   --  chip8.ads:73:4:AddI
+
+   procedure Test_LdFV_b0045b (Gnattest_T : in out Test);
+   --  chip8.ads:74:4:LdFV
+
+   procedure Test_LdBV_8c68b8 (Gnattest_T : in out Test);
+   --  chip8.ads:75:4:LdBV
+
+   procedure Test_LdArrV_ddac27 (Gnattest_T : in out Test);
+   --  chip8.ads:76:4:LdArrV
+
+   procedure Test_LdVArr_074f30 (Gnattest_T : in out Test);
+   --  chip8.ads:77:4:LdVArr
+
+   procedure Test_SetPc_c6ff2d (Gnattest_T : in out Test);
+   --  chip8.ads:79:4:SetPc
+
+   procedure Test_AddToPc_ce9d01 (Gnattest_T : in out Test);
+   --  chip8.ads:80:4:AddToPc
+
+   procedure Test_GetInstructionValue_0dbab7 (Gnattest_T : in out Test);
+   --  chip8.ads:81:4:GetInstructionValue
+
+   procedure Test_ConditionalJump_061d19 (Gnattest_T : in out Test);
+   --  chip8.ads:83:4:ConditionalJump
+
+end Chip8.Test_Data.Tests;
+--  end read only
---
+++ gnatest/tests/roms-test_data-tests.adb
@@ -0,0 +1,62 @@
+--  This package has been generated automatically by GNATtest.
+--  You are allowed to add your code to the bodies of test routines.
+--  Such changes will be kept during further regeneration of this file.
+--  All code placed outside of test routine bodies will be lost. The
+--  code intended to set up and tear down the test environment should be
+--  placed into roms.Test_Data.
+
+with AUnit.Assertions; use AUnit.Assertions;
+with System.Assertions;
+
+--  begin read only
+--  id:2.2/00/
+--
+--  This section can be used to add with clauses if necessary.
+--
+--  end read only
+
+--  begin read only
+--  end read only
+package body roms.Test_Data.Tests is
+
+--  begin read only
+--  id:2.2/01/
+--
+--  This section can be used to add global variables and other elements.
+--
+--  end read only
+
+--  begin read only
+--  end read only
+
+--  begin read only
+   procedure Test_pong (Gnattest_T : in out Test);
+   procedure Test_pong_e6d9d6 (Gnattest_T : in out Test) renames Test_pong;
+--  id:2.2/e6d9d6d3d1fa500f/pong/1/0/
+   procedure Test_pong (Gnattest_T : in out Test) is
+   --  roms.ads:3:4:pong
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_pong;
+--  end read only
+
+--  begin read only
+--  id:2.2/02/
+--
+--  This section can be used to add elaboration code for the global state.
+--
+begin
+--  end read only
+   null;
+--  begin read only
+--  end read only
+end roms.Test_Data.Tests;
