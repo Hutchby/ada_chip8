b91f63ee673f23981930fae7fa480e2dc2cdb3bd
Ricimello Lucien <ricime_l@epita.fr>
2018-01-31 16:33:32 +0100

    gui: refactor Main

--- src/gui.ads
+++ src/gui.ads
@@ -2,7 +2,7 @@ with HAL.Bitmap; use HAL.Bitmap;
 with Chip8; use Chip8;
 
 package gui is
-   type SpriteBuffer is array (0 .. 7, 0 .. 15) of Boolean;
+   type SpriteBuffer is array (0 .. 8, 0 .. 15) of Boolean;
    procedure pixel_draw (coord : Point; b : Boolean);
    procedure pixel_clr (coord : Point);
    function get_touch return Point;
@@ -10,8 +10,8 @@ package gui is
    procedure draw_button;
    procedure clr_screen;
    procedure setup_screen;
-   procedure draw_sprite (cpu : in out Chip8.Chip8; height : Natural; coord : Point);
+   procedure draw_sprite (screen_buff : in out FrameBuffer; height : Natural;
+                          buff : SpriteBuffer; coord : Point);
    procedure draw_screen (screen_buff : in FrameBuffer);
    procedure reset_screen (screen_buff : in out FrameBuffer);
-   function GetSprite (cpu : in Chip8.Chip8; height : Natural) return SpriteBuffer;
 end gui;
